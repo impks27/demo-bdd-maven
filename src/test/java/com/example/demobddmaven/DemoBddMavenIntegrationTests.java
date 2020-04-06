88b1b2dd58a8f60601f18de32b7dc36c66903d09
Pradeep Kumar Singh <raj.ratedr001@gmail.com>
2020-04-07 00:29:13 +0530

    create cucumber.json report

--- src/test/java/com/example/demobddmaven/DemoBddMavenIntegrationTests.java
+++ src/test/java/com/example/demobddmaven/DemoBddMavenIntegrationTests.java
@@ -6,7 +6,8 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features/bag.feature", plugin = {"pretty", "html:target/cucumber"})
+//@CucumberOptions(features = "src/test/resources/features/bag.feature", plugin = {"pretty", "html:target/cucumber"})
+@CucumberOptions(features = "src/test/resources/features/bag.feature", plugin = {"pretty", "html:target/cucumber", "json:target/surefire-reports/cucumber.json"})
 public class DemoBddMavenIntegrationTests {
 
 }
